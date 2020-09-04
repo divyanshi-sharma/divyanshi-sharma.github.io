7819cdcee300fccd4664fc16eb883e67bd919bdd
divyanshi-sharma <57505733+divyanshi-sharma@users.noreply.github.com>
2020-09-04 16:13:37 -0500

    build updates

---
+++ build/precache-manifest.b400a01d58fe133f0a9455f56ee5418d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6952817cbab732c4cbad7180e2f90f",
+    "url": "/divya-sharma/index.html"
+  },
+  {
+    "revision": "cd51cfcab800ea12ce65",
+    "url": "/divya-sharma/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "3b04469e16b8ea431503",
+    "url": "/divya-sharma/static/css/main.2656ebe9.chunk.css"
+  },
+  {
+    "revision": "cd51cfcab800ea12ce65",
+    "url": "/divya-sharma/static/js/2.547ce69d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/divya-sharma/static/js/2.547ce69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b04469e16b8ea431503",
+    "url": "/divya-sharma/static/js/main.51f69bcd.chunk.js"
+  },
+  {
+    "revision": "e36d6a04c2f1cf83314b",
+    "url": "/divya-sharma/static/js/runtime-main.caba0634.js"
+  },
+  {
+    "revision": "31c49332047e5326e190a32fc1eda603",
+    "url": "/divya-sharma/static/media/eventsImg.31c49332.png"
+  },
+  {
+    "revision": "10d6ff3a3b47fa5d78868b9f19533026",
+    "url": "/divya-sharma/static/media/farFromHomeImg.10d6ff3a.png"
+  },
+  {
+    "revision": "eb368bd44d497f6551feb9f0b8f343f1",
+    "url": "/divya-sharma/static/media/luraImg.eb368bd4.png"
+  },
+  {
+    "revision": "b183a55fb9d6e7c5711b70843c768261",
+    "url": "/divya-sharma/static/media/pinkLakePhoto.b183a55f.JPG"
+  },
+  {
+    "revision": "b305eab81dd57c481294faf635682ead",
+    "url": "/divya-sharma/static/media/selflogo2.b305eab8.png"
+  }
+]);
